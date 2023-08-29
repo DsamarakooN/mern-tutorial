1423fccad768d4a6ac5f7ae8b89738c6b6f48b62
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-08-30 04:35:34 +0530

    goal rout and controller setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,46 @@
+const asyncHandler = require('express-async-handler') // to handle async errors 
+// befor use need to install express-async-handler
+
+
 // @desc Get all goals
 // @route GET /api/goals
 // @access private
-const getGoals = (req, res) => {
+// const getGoals =(req, res) => {
+//     res.status(200).json({message: 'Get goals'})
+// }
+
+const getGoals = asyncHandler( async (req, res) => {
     res.status(200).json({message: 'Get goals'})
-}
+})
 
 // @desc set goal
 // @route POST /api/goals
 // @access private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler( async (req, res) => {
+
+    // to get data from body
+    //console.log(req.body);
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please enter a text field')
+    }
+
     res.status(200).json({message: 'Set goal'})
-}
+})
 
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @access private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler( async (req, res) => {
     res.status(200).json({message: `Updated goal ${req.params.id}`})
-}
+})
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler( async (req, res) => {
     res.status(200).json({message: `Deleted goal ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+// error middleware
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is up running on port ${port}`);
