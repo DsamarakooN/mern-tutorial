27b7b1bfdf20faa6fb9d8447babb2c77d2dbd178
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-08-29 22:53:26 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+    res.status(200).json({message: 'get goals'});
+});
+
+app.listen(port, () => {
+    console.log(`Server is up running on port ${port}`);
+});
