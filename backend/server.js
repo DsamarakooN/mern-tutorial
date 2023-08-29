2c2a22a6a350bfb70cf69e286f60c6d8a76861f0
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-08-30 05:13:32 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware');
 
+// connect to database
+const connectDB = require('./config/db');
+
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
