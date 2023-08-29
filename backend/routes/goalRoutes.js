fcd46992c270a1e6a34cf6244e2c077e35e80855
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-08-30 01:55:01 +0530

    routes updated

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getGoals, setGoal , updateGoal, deleteGoals } = require('../controllers/goalController');
+const { 
+    getGoals, 
+    setGoal, 
+    updateGoal, 
+    deleteGoals 
+} = require('../controllers/goalController');
 
 // insted of using below callback function, we can use controller 
 
@@ -8,24 +13,31 @@ const { getGoals, setGoal , updateGoal, deleteGoals } = require('../controllers/
 //     res.status(200).json({message: 'Get goals'})
 // })
 
-router.get('/', getGoals)
+// router.get('/', getGoals) (1)
 
 // router.post('/', (req, res) => {
 //     res.status(200).json({message: 'Set goals'})
 // })
 
-router.post('/', setGoal)
+// router.post('/', setGoal) (2)
+
+// we can set both (1) and (2) in one line as below
+
+router.route('/').get(getGoals).post(setGoal)
 
 // router.put('/:id', (req, res) => {
 //     res.status(200).json({message: `Update goal ${req.params.id}`})
 // })
 
-router.put('/:id', updateGoal)
+// router.put('/:id', updateGoal) (3)
 
 // router.delete('/:id', (req, res) => {
 //     res.status(200).json({message: `Delete goal ${req.params.id}`})
 // })
 
-router.delete('/:id', deleteGoals)
+// router.delete('/:id', deleteGoals)
+
+// we can set both (3) and (4) in one line as below
+router.route('/:id').put(updateGoal).delete(deleteGoals)
 
 module.exports = router;
