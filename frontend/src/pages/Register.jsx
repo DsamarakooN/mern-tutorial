9507f838c68c02b6c13412a55f839909d1234433
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-08-31 23:20:40 +0530

    registration form initiated

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,14 @@
+import { useState, useEffect } from 'react'
+
 function Register() {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    })
+
   return (
     <div>Register</div>
   )
