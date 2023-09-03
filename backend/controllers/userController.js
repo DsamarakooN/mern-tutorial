d066448855a9c1359f64c69e6d9789c99b647494
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-09-03 21:08:30 +0530

    user controller updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -91,18 +91,9 @@ const loginUser = asyncHandler(async (req, res) => {
 // @access private
 const getUserProfile = asyncHandler(async (req, res) => {
 
-    const {
-        _id,
-        name,
-        email
-    } = await User.findById(req.user.id)
+    
 
-    res.status(200).json({
-        id: _id,
-        name,
-        email,
-
-    })
+    res.status(200).json(req.user)
 })
 
 
